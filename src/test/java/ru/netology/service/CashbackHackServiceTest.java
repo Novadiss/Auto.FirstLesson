89c069e238baf5376addcca76a4a09793614e91c
Vladimir Palamar <vladimir.palamar@lenta.com>
2023-11-04 13:56:03 +0300

    chore: create CashbackHackServiceTest and add tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,23 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    void shouldCashbackLessThen1000() {
+        Assert.assertEquals(service.remain(900), 100);
+    }
+
+    @Test
+    void shouldCashbackMoreThen1000() {
+        Assert.assertEquals(service.remain(1_100), 900);
+    }
+
+    @Test
+    void shouldCashbackEquals1000() {
+        Assert.assertEquals(service.remain(1_000), 1_000);
+    }
+}
